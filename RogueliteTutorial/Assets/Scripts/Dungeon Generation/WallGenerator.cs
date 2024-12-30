bb07a42982624898391a2e067d4d3d70fb57c353
jacquelynnehei1 <jacquelynne.heiman@gmail.com>
2024-12-30 11:16:57 -0800

    Updated my dungeon to add all of the wall tile configurations to my WallGenerator and WallTypesHelper classes. This will ensure all of my walls are rendered correctly.

--- RogueliteTutorial/Assets/Scripts/Dungeon Generation/WallGenerator.cs
+++ RogueliteTutorial/Assets/Scripts/Dungeon Generation/WallGenerator.cs
@@ -22,6 +22,39 @@ public class WallGenerator
     public TileBase topLeftInnerCornerTile;
     public TileBase bottomRightInnerCornerTile;
     public TileBase bottomLeftInnerCornerTile;
+    public TileBase rightWallInnerEndTopTile;
+    public TileBase rightWallInnerEndBottomTile;
+    public TileBase rightWallInnerCenterTile;
+    public TileBase topWallTTile;
+    public TileBase bottomWallTTile;
+    public TileBase rightWallTTile;
+    public TileBase leftWallTTile;
+    public TileBase topRightConnectionLeftBorderTile;
+    public TileBase topRightConnectionBottomBorderTile;
+    public TileBase topLeftConnectionRightBorderTile;
+    public TileBase topLeftConnectionBottomBorderTile;
+    public TileBase bottomLeftConnectionTopBorderTile;
+    public TileBase bottomRightConnectionTopBorderTile;
+    public TileBase bottomRightConnectionLeftBorderTile;
+    public TileBase bottomLeftConnectionRightBorderTile;
+    public TileBase topWallInnerCenterTile;
+    public TileBase topWallInnerEndLeftTile;
+    public TileBase topWallInnerEndRightTile;
+    public TileBase topRightInteriorCornerTile;
+    public TileBase topLeftInteriorCornerTile;
+    public TileBase bottomRightInteriorCornerTile;
+    public TileBase bottomLeftInteriorCornerTile;
+    public TileBase bottomLeftTopRightConnectionTile;
+    public TileBase topLeftBottomRightConnectionTile;
+    public TileBase topRightConnectionTile;
+    public TileBase topLeftConnectionTile;
+    public TileBase bottomRightConnectionTile;
+    public TileBase bottomLeftConnectionTile;
+    public TileBase singleTile;
+    public TileBase bottomInteriorTTile;
+    public TileBase topInteriorTTile;
+    public TileBase leftInteriorTTile;
+    public TileBase rightInteriorTTile;
 
     private List<Vector2Int> directions = new List<Vector2Int>
     {
@@ -163,6 +196,138 @@ public class WallGenerator
         else if (WallTypesHelper.bottomRightInnerCorner.Contains(tileType))
         {
             return bottomRightInnerCornerTile;
+        }
+        else if (WallTypesHelper.rightWallInnerEndTop.Contains(tileType))
+        {
+            return rightWallInnerEndTopTile;
+        }
+        else if (WallTypesHelper.rightWallInnerEndBottom.Contains(tileType))
+        {
+            return rightWallInnerEndBottomTile;
+        }
+        else if (WallTypesHelper.rightWallInnerCenter.Contains(tileType))
+        {
+            return rightWallInnerCenterTile;
+        }
+        else if (WallTypesHelper.topWallT.Contains(tileType))
+        {
+            return topWallTTile;
+        }
+        else if (WallTypesHelper.bottomWallT.Contains(tileType))
+        {
+            return bottomWallTTile;
+        }
+        else if (WallTypesHelper.rightWallT.Contains(tileType))
+        {
+            return rightWallTTile;
+        }
+        else if (WallTypesHelper.leftWallT.Contains(tileType))
+        {
+            return leftWallTTile;
+        }
+        else if (WallTypesHelper.topRightConnectionLeftBorder.Contains(tileType))
+        {
+            return topRightConnectionLeftBorderTile;
+        }
+        else if (WallTypesHelper.topRightConnectionBottomBorder.Contains(tileType))
+        {
+            return topRightConnectionBottomBorderTile;
+        }
+        else if (WallTypesHelper.topLeftConnectionRightBorder.Contains(tileType))
+        {
+            return topLeftConnectionRightBorderTile;
+        }
+        else if (WallTypesHelper.topLeftConnectionBottomBorder.Contains(tileType))
+        {
+            return topLeftConnectionBottomBorderTile;
+        }
+        else if (WallTypesHelper.bottomLeftConnectionTopBorder.Contains(tileType))
+        {
+            return bottomLeftConnectionTopBorderTile;
+        }
+        else if (WallTypesHelper.bottomRightConnectionTopBorder.Contains(tileType))
+        {
+            return bottomRightConnectionTopBorderTile;
+        }
+        else if (WallTypesHelper.bottomRightConnectionLeftBorder.Contains(tileType))
+        {
+            return bottomRightConnectionLeftBorderTile;
+        }
+        else if (WallTypesHelper.bottomLeftConnectionRightBorder.Contains(tileType))
+        {
+            return bottomLeftConnectionRightBorderTile;
+        }
+        else if (WallTypesHelper.topWallInnerCenter.Contains(tileType))
+        {
+            return topWallInnerCenterTile;
+        }
+        else if (WallTypesHelper.topWallInnerEndLeft.Contains(tileType))
+        {
+            return topWallInnerEndLeftTile;
+        }
+        else if (WallTypesHelper.topWallInnerEndRight.Contains(tileType))
+        {
+            return topWallInnerEndRightTile;
+        }
+        else if (WallTypesHelper.topRightInteriorCorner.Contains(tileType))
+        {
+            return topRightInteriorCornerTile;
+        }
+        else if (WallTypesHelper.topLeftInteriorCorner.Contains(tileType))
+        {
+            return topLeftInteriorCornerTile;
+        }
+        else if (WallTypesHelper.bottomRightInteriorCorner.Contains(tileType))
+        {
+            return bottomRightInteriorCornerTile;
+        }
+        else if (WallTypesHelper.bottomLeftInteriorCorner.Contains(tileType))
+        {
+            return bottomLeftInteriorCornerTile;
+        }
+        else if (WallTypesHelper.bottomLeftTopRightConnection.Contains(tileType))
+        {
+            return bottomLeftTopRightConnectionTile;
+        }
+        else if (WallTypesHelper.topLeftBottomRightConnection.Contains(tileType))
+        {
+            return topLeftBottomRightConnectionTile;
+        }
+        else if (WallTypesHelper.topRightConnection.Contains(tileType))
+        {
+            return topRightConnectionTile;
+        }
+        else if (WallTypesHelper.topLeftConnection.Contains(tileType))
+        {
+            return topLeftConnectionTile;
+        }
+        else if (WallTypesHelper.bottomRightConnection.Contains(tileType))
+        {
+            return bottomRightConnectionTile;
+        }
+        else if (WallTypesHelper.bottomLeftConnection.Contains(tileType))
+        {
+            return bottomLeftConnectionTile;
+        }
+        else if (WallTypesHelper.single.Contains(tileType))
+        {
+            return singleTile;
+        }
+        else if (WallTypesHelper.bottomInteriorT.Contains(tileType))
+        {
+            return bottomInteriorTTile;
+        }
+        else if (WallTypesHelper.topInteriorT.Contains(tileType))
+        {
+            return topInteriorTTile;
+        }
+        else if (WallTypesHelper.leftInteriorT.Contains(tileType))
+        {
+            return leftInteriorTTile;
+        }
+        else if (WallTypesHelper.rightInteriorT.Contains(tileType))
+        {
+            return rightInteriorTTile;
         }
 
         return defaultWallTile;
